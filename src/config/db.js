d19ee102c925616627110118c8fff3d2203837e3
Diego Barletta <diegohbarletta@gmail.com>
2025-07-12 22:46:06 -0300

    env example file and db connection to firebase

--- src/config/db.js
+++ src/config/db.js
@@ -1,1 +1,27 @@
+import {config} from 'dotenv';
+config();
+import { getFirestore } from 'firebase/firestore';
+//import { initializeApp } from 'firebase/app';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+const db = getFirestore(app);
+export {db};
