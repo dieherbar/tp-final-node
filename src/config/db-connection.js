84d1a458f4a5836a5dfb0a14d44a5048734c2303
Diego Barletta <diegohbarletta@gmail.com>
2025-07-24 22:19:15 -0300

    cambios sugeridos utilizar envs importado en db.js

--- src/config/db-connection.js
+++ src/config/db-connection.js
@@ -1,15 +1,15 @@
-import {config} from 'dotenv';
-config();
 import { getFirestore } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
+import {envs} from './envs.js';
+
 
 const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERID,
-  appId: process.env.APPID
+  apiKey: envs.database.apikey,
+  authDomain: envs.database.authDomain,
+  projectId: envs.database.projectId,
+  storageBucket: envs.database.storageBucket,
+  messagingSenderId: envs.database.messagingSenderId,
+  appId: envs.database.appId
 };
 
 // Initialize Firebase
